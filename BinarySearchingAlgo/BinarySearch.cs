d9d9971c1b250e91c1135adc6aa580c979818a60
Mohith Reddy <50856290+Mohithbunny291@users.noreply.github.com>
2024-01-09 15:08:14 +0530

    Add project files.

---
+++ BinarySearchingAlgo/BinarySearch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchingAlgo
+{
+    internal class BinarySearch
+    {
+        //public BinarySearch()
+        //{
+        //    Console.WriteLine("Enter Number Of Elements");
+        //    int n = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"Enter {n} elements in Assending order");
+        //    int[] arr = new int[n];
+        //    foreach (int i in arr)
+        //    {
+        //        arr[i] = int.Parse(Console.ReadLine());
+        //    }
+
+        //    Console.WriteLine("Enter the element to be searched");
+        //    int key = int.Parse(Console.ReadLine());
+
+        //    Console.WriteLine(BinarySearchAlgo(arr, key));
+        //}
+        public int BinarySearchAlgo(int[] arr, int key)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int midNum = arr[mid];
+
+                if(key == midNum)
+                {
+                    return mid;
+                }
+                if(key > midNum)
+                {
+                    low = mid + 1;
+                }else
+                {   
+                    high = mid - 1;
+                }
+            }return -1;
+        }
+    }
+}
---
+++ BinarySearchingAlgo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchingAlgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            Console.WriteLine("Enter Number Of Elements");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter {n} elements in Assending order");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter the element to be searched");
+            int key = int.Parse(Console.ReadLine());
+            BinarySearch binarySearch = new BinarySearch();
+
+            Console.WriteLine(binarySearch.BinarySearchAlgo(arr, key));
+            }
+    }
+}
